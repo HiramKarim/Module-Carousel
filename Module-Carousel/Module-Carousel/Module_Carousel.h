5a26fdf4363c621ed86ef24eac63e81237865e50
Hiram Castro <hiram2027@gmail.com>
2023-04-18 22:03:11 -0700

    first init

---
+++ Module-Carousel/Module-Carousel/Module_Carousel.h
@@ -0,0 +1,18 @@
+//
+//  Module_Carousel.h
+//  Module-Carousel
+//
+//  Created by Hiram Castro on 18/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Module_Carousel.
+FOUNDATION_EXPORT double Module_CarouselVersionNumber;
+
+//! Project version string for Module_Carousel.
+FOUNDATION_EXPORT const unsigned char Module_CarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Module_Carousel/PublicHeader.h>
+
+
